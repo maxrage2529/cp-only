98c942bae4f1c641236f25759e2e88e14f40c980
maxrage2529 <71651048+maxrage2529@users.noreply.github.com>
2022-11-01 22:54:27 +0530

    Added unsafe route to check contents of database

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from werkzeug import Response
 from funcationalities.problemLinks import problems
 from flask import Flask, jsonify, request
 import time
+import os
 
 app = Flask(__name__)
+
+DATABSE_PATH = os.path.join(os.path.dirname(
+    __file__), "funcationalities", "database")
 
 
 @app.route('/')
@@ -15,22 +19,28 @@ def hello_world():
 def callProblems():
     start = time.time()
     userlist = request.args.get('userlist').split(',')
-    userlist=list(map(lambda x: x.lower(), userlist))
+    userlist = list(map(lambda x: x.lower(), userlist))
     low = int(request.args.get('low'))
     high = int(request.args.get('high'))
     need = int(request.args.get('need'))
-    #refresh argument input lena hai
+    # refresh argument input lena hai
     obj = problems(False)
     temp = obj.getProblemLinks(low, high, userlist, need)
     print(f"{time.time() - start} s for request")
     return jsonify(temp)
 
 
+@app.route("/files")
+def getFiles():
+    itemsList = os.listdir(DATABSE_PATH)
+    return jsonify(itemsList)
+
+
 @app.route("/test")
 def test():
     res = ""
-    for k,v in request.args.items() :
-        res+=f"{k} : {v}\n"
+    for k, v in request.args.items():
+        res += f"{k} : {v}\n"
     return Response(res)
 
 
--- funcationalities/problemLinks.py
+++ funcationalities/problemLinks.py
@@ -26,38 +26,43 @@ import os
 
 class problems:
     dirname = os.path.dirname(__file__)
-    def __init__(self,forceReload):
+
+    def __init__(self, forceReload):
+        if not os.path.exists(os.path.join(self.dirname, "database")):
+            os.mkdir(os.path.join(self.dirname, "database"))
         start = time.time()
         filename = os.path.join(self.dirname, "database\problemCounts_df.csv")
-        if(not os.path.exists(filename)):
-            pd.DataFrame({"username":[],"count":[]}).to_csv(filename,index=False)
-        self.problemCounts_df=pd.read_csv(filename)
+        if (not os.path.exists(filename)):
+            pd.DataFrame({"username": [], "count": []}
+                         ).to_csv(filename, index=False)
+        self.problemCounts_df = pd.read_csv(filename)
         filename = os.path.join(self.dirname, 'database\mainProblems_df.csv')
-        if((not os.path.exists(filename)) or forceReload):    
+        if ((not os.path.exists(filename)) or forceReload):
             self.refresh()
-        self.allProblems_df=pd.read_csv(filename)
+        self.allProblems_df = pd.read_csv(filename)
         print(f"{time.time() - start} initialisation compteted")
 
     def refresh(self):
         try:
             start = time.time()
-            filename = os.path.join(self.dirname, 'database\mainProblems_df.csv')
+            filename = os.path.join(
+                self.dirname, 'database\mainProblems_df.csv')
             problemset_problems = json.loads(requests.get(
                 "https://codeforces.com/api/problemset.problems").text)
             mainProblems = problemset_problems["result"]["problems"]
             mainProblems_df = pd.DataFrame(mainProblems)
             mainProblems_df.drop(
                 ["name", "type", "points"], axis=1, inplace=True)
-            
-            mainProblems_df.to_csv(filename,index=False)
-                
+
+            mainProblems_df.to_csv(filename, index=False)
+
             print(f"{time.time() - start} refresh completed")
         except:
             self.refresh()
 
-    def getUserProblemsFunction(self, user,startFrom=1,count=100000):
+    def getUserProblemsFunction(self, user, startFrom=1, count=100000):
         try:
-            #time is directly proportional to count
+            # time is directly proportional to count
             # print(user,startFrom,count)
             # print("https://codeforces.com/api/user.status?handle="+user+"&from="+str(startFrom)+"&count="+str(count))
             start = time.time()
@@ -65,67 +70,76 @@ class problems:
                 "https://codeforces.com/api/user.status?handle="+user+"&from="+str(startFrom)+"&count="+str(count))
             # print(res.text[:5])
             # print(res.status_code)
-            #if time starts from here total time will be near to 0.04
+            # if time starts from here total time will be near to 0.04
             self.user_status = json.loads(res.text)
         except:
-            self.getUserProblemsFunction(user,startFrom,count)
-        if(self.user_status["status"]=="FAILED" or len(self.user_status["result"])==0):
+            self.getUserProblemsFunction(user, startFrom, count)
+        if (self.user_status["status"] == "FAILED" or len(self.user_status["result"]) == 0):
             return pd.DataFrame([])
         temp_userProblems = self.user_status["result"]
         temp_userProblems_df = pd.DataFrame(temp_userProblems)
-        
+
         # here verdict has been ignored all problems has been included irrespective of result whether accpeted or not
         userProblems_df = pd.DataFrame(
             temp_userProblems_df["problem"].to_dict())
         userProblems_df = userProblems_df.transpose()
-        
-        newCount=userProblems_df.shape[0]
-        
-        if(startFrom==1):
+
+        newCount = userProblems_df.shape[0]
+
+        if (startFrom == 1):
             # self.problemCounts_df=self.problemCounts_df.append([{"username":user,"count":newCount}],ignore_index=True)
             print(self.problemCounts_df)
-            if(self.problemCounts_df.loc[self.problemCounts_df["username"]==user,"count"].shape[0]>0):
-                self.problemCounts_df.loc[self.problemCounts_df["username"]==user,"count"]+=newCount-1
+            if (self.problemCounts_df.loc[self.problemCounts_df["username"] == user, "count"].shape[0] > 0):
+                self.problemCounts_df.loc[self.problemCounts_df["username"]
+                                          == user, "count"] += newCount-1
             else:
-                self.problemCounts_df=pd.concat([self.problemCounts_df,pd.DataFrame([{"username":user,"count":newCount}])],ignore_index=True)
-        
-    
+                self.problemCounts_df = pd.concat([self.problemCounts_df, pd.DataFrame(
+                    [{"username": user, "count": newCount}])], ignore_index=True)
+
         filename = os.path.join(self.dirname, "database\problemCounts_df.csv")
-        self.problemCounts_df.to_csv(filename,index=False)
+        self.problemCounts_df.to_csv(filename, index=False)
         # userProblems_df.drop(["name", "type", "points"], axis=1, inplace=True)
-        userProblems_df=userProblems_df[["contestId","index","rating","tags"]]
+        userProblems_df = userProblems_df[[
+            "contestId", "index", "rating", "tags"]]
         userProblems_df.drop_duplicates(
             subset=["contestId", "index"], inplace=True)
         print(f"{time.time() - start} s in getUserProblemsFunction")
         return userProblems_df
-        
-    def mergeUserProblems(self,user):
+
+    def mergeUserProblems(self, user):
         # try:
         filename = os.path.join(self.dirname, f"database\{user}.csv")
         print(os.path.exists(filename))
-        if(not os.path.exists(filename)):
+        if (not os.path.exists(filename)):
             try:
-                self.getUserProblemsFunction(user).to_csv(filename,index=False)
+                self.getUserProblemsFunction(
+                    user).to_csv(filename, index=False)
             except:
-                self.getUserProblemsFunction(user).to_csv(filename,index=False)
+                self.getUserProblemsFunction(
+                    user).to_csv(filename, index=False)
         else:
             print(user)
-            print(self.problemCounts_df.loc[self.problemCounts_df.username==user])
-            totalSize=self.problemCounts_df.loc[self.problemCounts_df.username==user]["count"].iloc[0]
+            print(
+                self.problemCounts_df.loc[self.problemCounts_df.username == user])
+            totalSize = self.problemCounts_df.loc[self.problemCounts_df.username ==
+                                                  user]["count"].iloc[0]
             # print(totalSize)
-            count=self.getUserProblemsFunction(user,totalSize+1).shape[0]
-            print(totalSize,count)
-            if(count>0):
-                filename = os.path.join(os.path.dirname(__file__), f"database\{user}.csv")
-                tempUser_df=pd.read_csv(filename)
-                newSolvedProblems=self.getUserProblemsFunction(user,1,count+1)
+            count = self.getUserProblemsFunction(user, totalSize+1).shape[0]
+            print(totalSize, count)
+            if (count > 0):
+                filename = os.path.join(os.path.dirname(
+                    __file__), f"database\{user}.csv")
+                tempUser_df = pd.read_csv(filename)
+                newSolvedProblems = self.getUserProblemsFunction(
+                    user, 1, count+1)
                 print(newSolvedProblems)
-                tempUser_df=pd.concat([ newSolvedProblems,tempUser_df],ignore_index=True)
-                print(tempUser_df)   
-                tempUser_df.to_csv(filename,index=False)
+                tempUser_df = pd.concat(
+                    [newSolvedProblems, tempUser_df], ignore_index=True)
+                print(tempUser_df)
+                tempUser_df.to_csv(filename, index=False)
         # except:
-        #     self.mergeUserProblems(user)        
-                
+        #     self.mergeUserProblems(user)
+
     def createUserProblems(self, userlist):
         # try:
         start = time.time()
@@ -135,21 +149,21 @@ class problems:
         self.userProblems_df = pd.read_csv(filename)
         for user in userlist[1:]:
             self.mergeUserProblems(user)
-            filename = os.path.join(self.dirname, f"database\{user}.csv")  
+            filename = os.path.join(self.dirname, f"database\{user}.csv")
             # print(pd.concat([self.userProblems_df, pd.read_csv(filename)]
-            #     ,ignore_index=True))  
-            self.userProblems_df = pd.concat([self.userProblems_df, pd.read_csv(filename)]
-                ,ignore_index=True)
-            self.userProblems_df=self.userProblems_df.drop_duplicates(subset=["contestId", "index"],ignore_index=True)
+            #     ,ignore_index=True))
+            self.userProblems_df = pd.concat(
+                [self.userProblems_df, pd.read_csv(filename)], ignore_index=True)
+            self.userProblems_df = self.userProblems_df.drop_duplicates(
+                subset=["contestId", "index"], ignore_index=True)
 
-        
-        
         # user="userProblems_df"
-        # filename = os.path.join(self.dirname, f"database\{user}.csv") 
+        # filename = os.path.join(self.dirname, f"database\{user}.csv")
         # self.userProblems_df.to_csv(filename,index=False)
         print(f"{time.time() - start} s in createUserProblems")
         # except:
         #     self.createUserProblems(userlist)
+
     def getProblemLinks(self, low, high, userlist, need):
         # try:
         start = time.time()
@@ -160,6 +174,7 @@ class problems:
         return self.allProblems_df.sample(need).loc[:, ["contestId", "index"]].to_dict()
         # except:
         #     self.getProblemLinks(self, low, high, userlist, need)
+
 
 if __name__ == "__main__":
     obj = problems(False)
